89d533b6aa94b2674f7d54c13ed23d8890515660
Ben Racicot <benjaminracicot@gmail.com>
2023-12-11 19:36:24 -0500

    formatting

--- projects/sortable/src/lib/drag-drop/components/tree-component/nested-drag-drop.component.ts
+++ projects/sortable/src/lib/drag-drop/components/tree-component/nested-drag-drop.component.ts
@@ -1,9 +1,23 @@
 import { NgFor, NgIf, NgStyle, NgTemplateOutlet } from '@angular/common';
-import { Component, ElementRef, EventEmitter, HostListener, Input, Output, QueryList, SimpleChanges, TemplateRef, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  QueryList,
+  SimpleChanges,
+  TemplateRef,
+  ViewChild,
+  ViewChildren,
+  ViewEncapsulation,
+} from '@angular/core';
+import { NgClass } from '@angular/common';
 import { asapScheduler } from 'rxjs';
+
 import { CdkDragDrop, CdkDragNest, CdkNestDrop } from '../../drag-events';
 
-import { NgClass } from '@angular/common';
 import { CdkDrag } from '../../directives/drag';
 import { CdkDragHandle } from '../../directives/drag-handle';
 import { CdkDragPlaceholder } from '../../directives/drag-placeholder';
@@ -11,17 +25,14 @@ import { CdkDragPreview } from '../../directives/drag-preview';
 import { CdkDropList } from '../../directives/drop-list';
 import { CdkDropListGroup } from '../../directives/drop-list-group';
 import { DragDrop } from '../../drag-drop';
-// import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import {
-  transferArrayItem
-} from '../../drag-utils';
+import { transferArrayItem } from '../../drag-utils';
 
 export type IPageMode = 'VIRTUAL-SCROLL' | 'PAGINATION';
 
-import { CdkDropDownItem, getParent, } from '../../drag-drop-tree';
+import { CdkDropDownItem, getParent } from '../../drag-drop-tree';
 
-
-const TAG = "nested-drag-drop.component.ts";
+// unused?
+const TAG = 'nested-drag-drop.component.ts';
 
 /**
  * @value Drag&Drop connected sorting group
@@ -45,11 +56,9 @@ const TAG = "nested-drag-drop.component.ts";
     NgClass,
     NgStyle,
   ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class CdkNestedDragDropComponent {
-
-
   @Input('cdkNestedDropDownData')
   itemTreeList: CdkDropDownItem[] = [];
 
@@ -63,14 +72,11 @@ export class CdkNestedDragDropComponent {
 
   @Input('height') style_height: string = '100%';
 
+  @Output('cdkDragDropped') readonly dropped: EventEmitter<CdkNestDrop> =
+    new EventEmitter<CdkNestDrop>();
 
-  @Output('cdkDragDropped') readonly dropped: EventEmitter<CdkNestDrop> = new EventEmitter<
-    CdkNestDrop
-  >();
-
-
-  @Output('cdkScrollNextPage') readonly scrollNextPage: EventEmitter<any> = new EventEmitter<any>();
-
+  @Output('cdkScrollNextPage') readonly scrollNextPage: EventEmitter<any> =
+    new EventEmitter<any>();
 
   @ViewChild('scrollBox') scrollBox!: ElementRef<HTMLElement>;
   @ViewChild('scrollGap') scrollGap!: ElementRef<HTMLElement>;
@@ -81,20 +87,71 @@ export class CdkNestedDragDropComponent {
 
   dropLists: CdkDropList[] = [];
   dropGutterSize = 0;
+  isLoading = false;
+  showGap: boolean = true;
+
+  @HostListener('window:scroll', ['$event']) onScroll(event: Event) {
+    if (this.pageMode == 'VIRTUAL-SCROLL') {
+      if (
+        !this.isLoading &&
+        this.showGap &&
+        document.scrollingElement &&
+        document.scrollingElement?.scrollHeight -
+          document.scrollingElement?.scrollTop <=
+          document.scrollingElement?.clientHeight + 1
+      ) {
+        this.isLoading = true;
+        this.scrollNextPage.emit(event);
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    const ldls: CdkDropList[] = [];
+    this.dlq.forEach((dl) => {
+      ldls.push(dl);
+    });
+
+    asapScheduler.schedule(() => {
+      this.dropLists = ldls;
+    });
+  }
+
+  ngAfterContentChecked() {
+    if (this.pageMode == 'VIRTUAL-SCROLL') {
+      this.showGap = true;
+    } else if (this.pageMode == 'PAGINATION') {
+      this.showGap = false;
+    }
+  }
 
   drop(event: CdkDragDrop<any>) {
-
     let prevListData: CdkDropDownItem[] = event.previousContainer.data;
     let curListData: CdkDropDownItem[] = event.container.data;
 
-    const previousIndex = prevListData.length > 0 && prevListData[0]['_isEmpty'] == true ? event.previousIndex + 1 : event.previousIndex;
-    const currentIndex = curListData.length > 0 && curListData[0]['_isEmpty'] == true ? event.currentIndex + 1 : event.currentIndex;
-    let nestIndex = -1;
+    const previousIndex =
+      prevListData.length > 0 && prevListData[0]['_isEmpty'] == true
+        ? event.previousIndex + 1
+        : event.previousIndex;
+    const currentIndex =
+      curListData.length > 0 && curListData[0]['_isEmpty'] == true
+        ? event.currentIndex + 1
+        : event.currentIndex;
 
+    let nestIndex = -1;
     let nestInfo = event.nestInfo;
-    if (nestInfo && nestInfo.nestIndex < curListData.length && nestInfo.nestIndex >= 0) {
+
+    if (
+      nestInfo &&
+      nestInfo.nestIndex < curListData.length &&
+      nestInfo.nestIndex >= 0
+    ) {
       nestIndex = nestInfo.nestIndex;
-      curListData.length > 0 && curListData[0]['_isEmpty'] == true && (nestIndex = nestIndex + 1);
+      curListData.length > 0 &&
+        curListData[0]['_isEmpty'] == true &&
+        (nestIndex = nestIndex + 1);
     }
 
     if (nestIndex >= 0) {
@@ -106,7 +163,7 @@ export class CdkNestedDragDropComponent {
         item: prevListData[previousIndex],
         parent: targetItem,
         position: targetChildren.length,
-        isNesting: true
+        isNesting: true,
       });
       // END OF SENDING MESSAGE
 
@@ -119,83 +176,23 @@ export class CdkNestedDragDropComponent {
       targetItem.children = targetChildren;
     } else {
       // BEGIN OF SENDING MESSAGE
-      const parent = getParent(this.itemTreeList, curListData)
-        ;
+      const parent = getParent(this.itemTreeList, curListData);
       this.dropped.emit({
         item: prevListData[previousIndex],
         parent: parent,
         position: currentIndex,
-        isNesting: false
+        isNesting: false,
       });
       // END OF SENDING MESSAGE
 
-      transferArrayItem(
-        prevListData,
-        curListData,
-        previousIndex,
-        currentIndex
-      );
+      transferArrayItem(prevListData, curListData, previousIndex, currentIndex);
     }
-    // }
   }
 
-  nest(event: CdkDragNest<any>) {
-
-  }
+  nest(event: CdkDragNest<any>) {}
 
   getDropGutterSize(): number {
     this.dropGutterSize = 0;
     return 0;
   }
-
-  ngAfterViewInit() {
-    console.log("NgAfterViewinit");
-
-    const ldls: CdkDropList[] = [];
-    this.dlq.forEach((dl) => {
-      ldls.push(dl)
-    });
-
-    asapScheduler.schedule(() => { this.dropLists = ldls; });
-
-  }
-
-  isLoading = false;
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event) {
-    if (this.pageMode == 'VIRTUAL-SCROLL') {
-      if (!this.isLoading && this.showGap && document.scrollingElement && document.scrollingElement?.scrollHeight - document.scrollingElement?.scrollTop <= document.scrollingElement?.clientHeight + 1) {
-        this.isLoading = true;
-        this.scrollNextPage.emit(event);
-        console.log('Event', 'Touch Sroll End');
-      }
-    }
-  }
-
-  showGap: boolean = true;
-  ngAfterContentChecked() {
-    if (this.pageMode == 'VIRTUAL-SCROLL') {
-      this.showGap = true;
-    } else if (this.pageMode == 'PAGINATION') {
-      this.showGap = false;
-    }
-
-  }
-
-  ngOnChanges(change: SimpleChanges) {
-    // setTimeout(()=>{
-      this.isLoading = false;
-    // }, 1000);
-  }
-  
-
-  constructor() {
-  }
 }
-
-/**
-Copyright 2019 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
