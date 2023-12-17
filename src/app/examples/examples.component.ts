5884816f2617231be98e00b724f50653ed46fece
Ben Racicot <benjaminracicot@gmail.com>
2023-12-16 20:27:46 -0500

    updates

--- src/app/examples/examples.component.ts
+++ src/app/examples/examples.component.ts
@@ -1,8 +1,6 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
-// import {MatProgressSpinnerModule} from '@angular/material';
-// MatProgressSpinnerModule
+
 // our lib - a custom version of https://github.com/angular/components/tree/main/src/cdk/drag-drop
 import {
   CdkDrag,
@@ -45,14 +43,12 @@ import { CircularProgressComponent } from './circular-progressive.component';
     CdkDragPlaceholder,
     CdkNestedDragDropComponent,
     CircularProgressComponent,
-    MatPaginatorModule,
   ],
   providers: [DragDrop],
   standalone: true,
   styleUrls: ['./examples.component.scss'],
 })
-export class ExamplesComponent  implements OnInit  {
-
+export class ExamplesComponent implements OnInit {
   // Reordering lists example - https://material.angular.io/cdk/drag-drop/overview#reordering-lists
   movies = [
     'Episode I - The Phantom Menace',
@@ -65,32 +61,13 @@ export class ExamplesComponent  implements OnInit  {
     'Episode VIII - The Last Jedi',
     'Episode IX – The Rise of Skywalker',
   ];
-
-
-
-  reorderDrop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-  }
-
+  indexTree: CdkIndexTree;
+  currentPage: number = 0;
+  totalCount: number = 0;
+  pageSize: number = 10;
+  listItems: CdkDropDownItem[] = [];
+  isLoading = false;
   // nestItemClass = "cdk-nest-dragdrop-item";
-
-  //  TRANSFERRING ITEMS BETWEEN LISTS EXAMPLE - https://material.angular.io/cdk/drag-drop/overview#transferring-items-between-lists
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  transferDrop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }
-
 
   // List orientation - https://material.angular.io/cdk/drag-drop/overview#list-orientation
   timePeriods = [
@@ -101,111 +78,94 @@ export class ExamplesComponent  implements OnInit  {
     'Long nineteenth century',
   ];
 
-  horizontalDrop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
-  }
-
-
-
+  // TRANSFERRING ITEMS BETWEEN LISTS EXAMPLE - https://material.angular.io/cdk/drag-drop/overview#transferring-items-between-lists
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
   pageMode: IPageMode = 'VIRTUAL-SCROLL';
 
   dropdownTree: CdkDropDownItem[] = [
     {
-      name: "Section 1",
+      name: 'Section 1',
     },
     {
-      name: "Section 2",
+      name: 'Section 2',
       children: [
         {
-          name: "Section 2.1",
-          description: "This is a description about this section"
-
+          name: 'Section 2.1',
+          description: 'This is a description about this section',
         },
         {
-          name: "Section 2.2",
-          description: "This is a description about this section"
-
+          name: 'Section 2.2',
+          description: 'This is a description about this section',
         },
         {
-          name: "Section 2.3",
-          description: "This is a description about this section"
-
-        }
-      ]
+          name: 'Section 2.3',
+          description: 'This is a description about this section',
+        },
+      ],
     },
     {
-      name: "Section 3",
+      name: 'Section 3',
       children: [
         {
-          name: "Section 3.1",
-          description: "This is a description about this section"
+          name: 'Section 3.1',
+          description: 'This is a description about this section',
         },
         {
-          name: "Section 3.2",
-          description: "This is a description about this section",
+          name: 'Section 3.2',
+          description: 'This is a description about this section',
           children: [
             {
-              name: "Section 3.2.1",
-              description: "This is a description about this section"
-
+              name: 'Section 3.2.1',
+              description: 'This is a description about this section',
             },
             {
-              name: "Section 3.2.2",
-              description: "This is a description about this section"
-
+              name: 'Section 3.2.2',
+              description: 'This is a description about this section',
             },
             {
-              name: "Section 3.2.3",
-              description: "This is a description about this section",
+              name: 'Section 3.2.3',
+              description: 'This is a description about this section',
 
               children: [
                 {
-                  name: "Section 3.2.3.1",
-                  description: "This is a description about this section",
-
-
+                  name: 'Section 3.2.3.1',
+                  description: 'This is a description about this section',
                 },
                 {
-                  name: "Section 3.2.3.2",
-                  description: "This is a description about this section",
-
-
-                }
-              ]
-            }
-          ]
+                  name: 'Section 3.2.3.2',
+                  description: 'This is a description about this section',
+                },
+              ],
+            },
+          ],
         },
-      ]
+      ],
     },
     {
-      name: "Section 4",
-      description: "This is a description about this section",
-
-
+      name: 'Section 4',
+      description: 'This is a description about this section',
     },
     {
-      name: "Section 5",
-      description: "This is a description about this section",
-
-
+      name: 'Section 5',
+      description: 'This is a description about this section',
     },
     {
-      name: "Section 6",
-      description: "This is a description about this section",
-
-
+      name: 'Section 6',
+      description: 'This is a description about this section',
     },
   ];
 
   ngOnInit(): void {
     let items = [];
     let i, k, subItems = [];
-    for ( i = 0 ; i < 100; i++ ) {
+
+    for (i = 0; i < 100; i++) {
       subItems = [];
-      for ( k = 0; k < 0; k++ ) {
-        let item : any = {
-          name: `Section ${i+1}.${k+1}`,
-          description: "This is a description about this section",
+      for (k = 0; k < 0; k++) {
+        let item: any = {
+          name: `Section ${i + 1}.${k + 1}`,
+          description: 'This is a description about this section',
         };
         // if ( i % 2 == 0 ) {
         //   item = {...item, draggingDisabled: true }
@@ -213,85 +173,99 @@ export class ExamplesComponent  implements OnInit  {
         subItems.push(item);
       }
       items.push({
-        name: `Section ${i+1}`,
-        children: subItems
-      })
+        name: `Section ${i + 1}`,
+        children: subItems,
+      });
     }
     this.dropdownTree = items;
-  }
-
-  indexTree: CdkIndexTree;
-
-  currentPage: number = 0;
-
-  totalCount: number = 0;
-
-  pageSize: number = 10;
-
-  listItems: CdkDropDownItem[] = [];
-
-  isLoading = false;
-
-  onNestDragDropped(event: CdkNestDrop) {
-    console.log('drag&drop event :>> ', event);
-
-  }
-
-  pageChanged(event: number) {
-    this.isLoading = true;
-    setTimeout(() => {
-      const page: number = event;
-
-      let startIndex = page * this.pageSize;
-      let endIndex = Math.min(startIndex + this.pageSize - 1, this.totalCount - 1);
-
-      const splittedTree = splitTree(this.indexTree, startIndex, endIndex);
-
-      splittedTree.children && (this.listItems = splittedTree.children)
-
-      splittedTree.children && (this.currentPage = page);
-
-      this.isLoading = false;
-    }, 1250);
-
-
-  }
-
-  onScrollNextPage(event: Event) {
-    console.log('event :>> ', event);
-    this.isLoading = true;
-    setTimeout(() => {
-      // const page: number = this.listItems.length / this.pageSize + 2;
-      this.currentPage = this.currentPage + 1;
-      let page = this.currentPage;
-
-      let startIndex = 0;
-      let endIndex = Math.min(page * this.pageSize + this.pageSize - 1, this.totalCount - 1);
-
-      const splittedTree = splitTree(this.indexTree, startIndex, endIndex);
-
-      splittedTree.children && splittedTree.children.length > this.listItems.length && (this.listItems = splittedTree.children)
-
-      this.isLoading = false;
-
-    }, 250);
   }
 
   ngAfterContentInit() {
     const tree = buildTree(this.dropdownTree);
     this.totalCount = getDecendantCount(tree) - 1;
     this.indexTree = constructCdkIndexTree(tree);
-
     const splittedTree = splitTree(this.indexTree, 0, this.pageSize - 1);
 
-    splittedTree.children && (this.listItems = splittedTree.children)
-
-    splittedTree.children && (this.currentPage = 0)
-
+    splittedTree.children && (this.listItems = splittedTree.children);
+    splittedTree.children && (this.currentPage = 0);
   }
 
-  onPageUpdate(page: PageEvent) {
-    console.log('page :>> ', page);
-    this.listItems = this.dropdownTree.splice(page.pageIndex * page.pageSize, (page.pageIndex + 1) * page.pageSize - 1) ;
+  onNestDragDropped(event: CdkNestDrop) {
+    console.log('drag&drop event :>> ', event);
+  }
+
+  reorderDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
+
+  transferDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  horizontalDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+  }
+
+  pageChanged(event: number) {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      const page: number = event;
+      let startIndex = page * this.pageSize;
+      let endIndex = Math.min(
+        startIndex + this.pageSize - 1,
+        this.totalCount - 1
+      );
+
+      const splittedTree = splitTree(this.indexTree, startIndex, endIndex);
+      splittedTree.children && (this.listItems = splittedTree.children);
+      splittedTree.children && (this.currentPage = page);
+      this.isLoading = false;
+    }, 1250);
+  }
+
+  onScrollNextPage(event: Event) {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      // const page: number = this.listItems.length / this.pageSize + 2;
+      this.currentPage = this.currentPage + 1;
+      let page = this.currentPage;
+
+      let startIndex = 0;
+      let endIndex = Math.min(
+        page * this.pageSize + this.pageSize - 1,
+        this.totalCount - 1
+      );
+
+      const splittedTree = splitTree(this.indexTree, startIndex, endIndex);
+
+      // what is this?
+      if (splittedTree.children && splittedTree.children.length > this.listItems.length) {
+        this.listItems = splittedTree.children;
+      }
+
+      this.isLoading = false;
+    }, 250);
+  }
+
+
+  // what is PageEvent?
+  // onPageUpdate(page: PageEvent) {
+  onPageUpdate(page: any) {
+    this.listItems = this.dropdownTree.splice(page.pageIndex * page.pageSize, (page.pageIndex + 1) * page.pageSize - 1);
   }
 }
